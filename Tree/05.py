8c035c276b99ec75666e0f84df018a0c8b6728b6
Swaraj <swaraj.dehuri@gmail.com>
2024-06-24 14:55:48 +0530

    level order traversal naive way

---
+++ Tree/05.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return str(self.data)
+
+
+def height(node):
+    if not node:
+        return 0
+    
+    left_height = height(node.left)
+    right_height = height(node.right)
+
+    if left_height > right_height:
+        return left_height + 1
+    else:
+        return right_height + 1
+
+
+def print_current_level_nodes(node, level):
+    if not node:
+        return
+    
+    if level == 0:
+        print(node.data, end=" ")
+    else:
+        print_current_level_nodes(node.left, level - 1)
+        print_current_level_nodes(node.right, level - 1)
+
+
+################## NAIVE WAY #####################
+
+def level_order_traversal_naive(node):
+    h = height(node)
+
+    for i in range(h):
+        print_current_level_nodes(node, i)
+
+################## USING QUEUE ####################
+
+def level_order_traversal_queue(node):
+    pass
+
+
+root_node = Node(10)
+root_node.left = Node(20)
+root_node.left.left = Node(15)
+
+root_node.right = Node(30)
+root_node.right.left = Node(40)
+root_node.right.right = Node(80)
+root_node.right.right.right = Node(50)
+
+level_order_traversal_naive(root_node)
--- Tree/README.md
+++ Tree/README.md
@@ -4,4 +4,5 @@
 1. General Tree [Python code](https://github.com/skdehuri/DSA/blob/master/Tree/01.py)
 2. Binary Tree Traversal [Python code](https://github.com/skdehuri/DSA/blob/master/Tree/02.py) 
 3. Height of the Binary Tree [Python code](https://github.com/skdehuri/DSA/blob/master/Tree/03.py)
-3. Print all the nodes at K distance from root [Python code](https://github.com/skdehuri/DSA/blob/master/Tree/04.py)
+4. Print all the nodes at K distance from root [Python code](https://github.com/skdehuri/DSA/blob/master/Tree/04.py)
+5. Level Order Traversal [Python code](https://github.com/skdehuri/DSA/blob/master/Tree/05.py)
